7f5c13cb2f1c4f3e2a972760781095119961de3a
justice-udehegbunam <justicefavour491@gmail.com>
2023-11-05 22:10:24 +0100

    done at last!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./build/*.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
